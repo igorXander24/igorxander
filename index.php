132cd4ab1ac4b222eec83b8dd41d07598b297f7b
igorXander24 <xanderxd12@gmail.com>
2019-01-03 22:36:33 -0500

    Pruebass

--- index.php
+++ index.php
@@ -415,7 +415,7 @@
         var marker = new google.maps.Marker({position: uluru, map: map});
     }
 </script>
-<!--Load the API from the specified URL
+<!--Load the API from the specified URL pruebas
 * The async attribute allows the browser to render the page while the API loads
 * The key parameter will contain your own API key (which is not needed for this tutorial)
 * The callback parameter executes the initMap() function
