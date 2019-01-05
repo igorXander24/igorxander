6559a1ddfecd3743329598d2acc98b25cfe2126c
igorXander24 <xanderxd12@gmail.com>
2019-01-05 13:11:19 -0500

    segundas modificaciones

--- index.php
+++ index.php
@@ -622,7 +622,7 @@
     }
 
 </script>
-<!--Load the API from the specified URL igor alexander quispe
+<!--Load the API from the specified URL
 * The async attribute allows the browser to render the page while the API loads
 * The key parameter will contain your own API key (which is not needed for this tutorial)
 * The callback parameter executes the initMap() function
